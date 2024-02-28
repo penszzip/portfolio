9a6ea1cf34164c13f225643b5912d5c8660434e0
penszzip <pazhiloyflex@gmail.com>
2024-02-27 22:59:33 -0500

    added header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import { useState } from "react"
 import Header from "./components/Header.jsx"
 
 function App() {
 
+  const [darkMode, setDarkMode] = useState(false)
+
   return (
-    <div>
-      <Header />
+    <div className={darkMode && "dark"}>
+      <Header darkMode={darkMode} setDarkMode={setDarkMode} />
     </div>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { MdNightsStay, MdWbSunny } from "react-icons/md"
 
-const Header = () => {
+const Header = ({ darkMode, setDarkMode }) => {
   return (
-    <div>Header</div>
+    <header className='bg-white dark:bg-gray-900 text-gray-900 dark:text-white'>
+      <nav className="flex justify-between items-center px-5 py-5">
+        <h1>Portfolio</h1>
+
+        <div onClick={() => setDarkMode(!darkMode)}>
+          {darkMode ? (
+            <MdWbSunny className="text-2xl cursor-pointer" />
+          ) : (
+            <MdNightsStay className="text-2xl cursor-pointer" />
+          )}
+        </div>
+      </nav>
+    </header>
   )
 }
 
